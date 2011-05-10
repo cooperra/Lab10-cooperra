d37547fd222113276a18802c69ffbdf8ec579f01
Robbie Cooper <cooperra@rose-hulman.edu>
2011-05-09 21:09:50 -0400

    Complete T5

--- Fortune-cooperra/fortune.rb
+++ Fortune-cooperra/fortune.rb
@@ -1,6 +1,13 @@
-
 require 'sinatra/base'
 require 'haml'
 require 'thin'
 class Fortune < Sinatra::Base
+
+  get '/fortune/random/?' do
+    @fortune = $fortunes[rand($fortunes.size)]
+    haml :fortune
+  end
+
+  require 'fortunedata'
+
 end
