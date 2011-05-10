50209adf0d782e818385d4647a42892d8805d662
Robbie Cooper <cooperra@rose-hulman.edu>
2011-05-09 22:55:37 -0400

    Cucumber test complete?

--- Fortune-cooperra/features/step_definitions/fortune_steps.rb
+++ Fortune-cooperra/features/step_definitions/fortune_steps.rb
@@ -2,3 +2,8 @@ Then /^I should see a fortune$/ do
   fortune = page.find(".fortune")
   fortune.text.should match /^\n.*\n\s*$/
 end
+
+Then /^I should see the fortune \"(.*)\"$/ do |fortune|
+  fortune = page.find(".fortune")
+  fortune.text.should match fortune
+end
